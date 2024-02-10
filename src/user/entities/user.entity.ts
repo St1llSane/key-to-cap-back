01585eb8f3584ecfad120a7b68c278bc39f3f06a
st1llsane <notescape262@gmail.com>
2024-02-10 19:19:23 +0300

    chore: added columns for user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserGender } from './types';
 
 @Entity('users')
 export class User {
@@ -10,4 +11,22 @@ export class User {
 
   @Column({ name: 'password', type: 'varchar' })
   password: string;
+
+  @Column({ name: 'first_name', type: 'varchar', nullable: true })
+  firstName: string;
+
+  @Column({ name: 'last_name', type: 'varchar', nullable: true })
+  lastName: string;
+
+  @Column({ name: 'birth_date', type: 'timestamp', nullable: true })
+  birthDate: Date;
+
+  @Column({ name: 'gender', type: 'enum', enum: UserGender, nullable: true })
+  gender: UserGender | null;
+
+  @Column({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  @Column({ name: 'is_cookie_accepted', type: 'boolean', default: false })
+  isCookieAccepted: boolean;
 }
